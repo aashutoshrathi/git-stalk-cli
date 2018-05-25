2dbefef962bb57f11b98d06d7f0c60e794898c72
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-05-25 18:29:49 +0530

    version upgraded

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="git_stalk",
-    version="0.0.1",
+    version="0.1.1",
     author="Aashutosh Rathi",
     author_email="aashutoshrathi@gmail.com",
     description="For stalkers like Daddu",
