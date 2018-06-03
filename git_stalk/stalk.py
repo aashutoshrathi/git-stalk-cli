29c0d0e3c2de46d4134e6a7dd48cc3a862f9a3a3
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-06-04 01:45:01 +0530

    Enhancement and Added Help

--- git_stalk/stalk.py
+++ git_stalk/stalk.py
@@ -30,18 +30,24 @@ def get_event(string):
 def get_details(event):
     if event["type"] == "IssuesEvent":
         return event["payload"]["issue"]["title"]
-    if event["type"] == "IssueCommentEvent":
+    elif event["type"] == "IssueCommentEvent":
         return event["payload"]["comment"]["body"]
-    if event["type"] == "WatchEvent":
+    elif event["type"] == "WatchEvent":
         response = requests.get(event["repo"]["url"])
         repo = response.json()
         return repo["language"]
-    if event["type"] == "PullRequestEvent":
+    elif event["type"] == "PullRequestEvent":
         return event["payload"]["pull_request"]["title"]
-    if event["type"] == "PushEvent":
+    elif event["type"] == "PushEvent":
         for commit in event["payload"]["commits"]:
             if commit["distinct"]:
                 return commit["message"]
+    elif event["type"] == "MemberEvent":
+        return "Added " + event["payload"]["member"]["login"] + " as collaborator"
+    elif event["type"] == "ReleaseEvent":
+        return "Released binaries for version " + event["payload"]["release"]["tag_name"]
+    elif event["type"] == "ForkEvent":
+        return "Forked " + event["repo"]["name"]
 
 def check_for_fork(link, user):
     tukde = link.split('/')
@@ -60,13 +66,17 @@ def get_local_time(string):
     samay = tukde[1].split('+')[0]
     return samay
 
-def get_basic_info(profile):
+def get_basic_info(user):
+    user_link = "https://api.github.com/users/" + str(user)
+    user_profile = requests.get(user_link)
+    profile = user_profile.json()
     print("Name:", profile["name"])
     print("Company:", profile["company"])
+    print("Bio:", profile["bio"])
     print("Followers:", profile["followers"])
     print("Following:", profile["following"])
     print("Public Repos:", profile["public_repos"])
-    print("Contributions Today: ")
+    print()
 
 
 def convert_to_local(string):
@@ -77,51 +87,92 @@ def convert_to_local(string):
     return(str(local_stamp))
 
 
-def show_contri(user):
-    now = datetime.datetime.now()
-    today = str(now.strftime("%Y-%m-%d"))
-    # today = "2018-06-02"
-    link = "https://api.github.com/users/" + str(user) + "/events"
-    user_link = "https://api.github.com/users/" + str(user)
-
-    user_profile = requests.get(user_link)
-    profile = user_profile.json()
-
-    get_basic_info(profile)
-
-    response = requests.get(link)
-    events = response.json()
-    latest = []
-    stars = []
-    if response.status_code == 200:    
-        for event in events:
-            if convert_to_local(event["created_at"]).startswith(today) and event["type"] != "IssueCommentEvent":
-                if event["type"] == "WatchEvent":
-                    stars.append(event)
-                elif check_for_fork(event["repo"]["url"], user):
-                    latest.append(event)
-    
+def get_contributions(user, latest):
+    print("Contributions Today: ")
     if latest:
         table = PrettyTable(["Type", "Repository", "Time", "Details"])
         for event in latest:
             table.add_row([get_event(event["type"]), event["repo"]["name"], get_local_time(event["created_at"]), get_details(event)])
         print(table)
     print(user + " have made " + str(len(latest)) + " public contribution(s) today.\n")
-    
+
+
+def get_other_activity(user, other):
+    print("Other Activity today: ")
+    if other:
+        other_table = PrettyTable(["Type", "Repository", "Time", "Details"])
+        for event in other:
+            other_table.add_row([get_event(event["type"]), event["repo"]["name"], get_local_time(event["created_at"]), get_details(event)])
+        print(other_table)
+    print(user + " have done " + str(len(other)) + " other public activit(y/ies) today.\n")
+
+
+def get_stars(user, stars):
     print("Starred today: ")
     if stars:
         star_table = PrettyTable(["Repository", "Language", "Time"])
         for event in stars:
             star_table.add_row([event["repo"]["name"], get_details(event), get_local_time(event["created_at"])])
         print(star_table)
-    return (user + " have starred " + str(len(stars)) + " repo(s) today.")
+    print (user + " have starred " + str(len(stars)) + " repo(s) today.")
+
+
+def fill_data(user, today, events, latest, stars, other):
+    for event in events:
+        if convert_to_local(event["created_at"]).startswith(today) and event["type"] != "IssueCommentEvent":
+            if event["type"] == "WatchEvent":
+                stars.append(event)
+            elif event["type"] == "ForkEvent" or event["type"] == "MemberEvent":
+                other.append(event)
+            elif check_for_fork(event["repo"]["url"], user):
+                latest.append(event)
+    return latest, stars, other
+
+
+def show_help():
+    print("Usage: stalk [OPTIONS] username [USERNAME]")
+    print("Options:\n  General Options:")
+    print("    -h, --help                       Print this help text and exit")
+    # print("    --version                        Print program version and exit")
+    print("    -np                              Stalks a user without showing their profile")
+
+
+def show_contri(user, arg=None):
+    now = datetime.datetime.now()
+    today = str(now.strftime("%Y-%m-%d"))
+    today = "2018-06-03"
+    link = "https://api.github.com/users/" + str(user) + "/events"    
+    response = requests.get(link)
+    events = response.json()
+    latest = []
+    stars = []
+    other = []
+    if response.status_code == 200:    
+        latest, stars, other = fill_data(user, today, events, latest, stars, other)
+    else:
+        print("Something went wrong, check your internet or username. \nUse stalk --help for Help")
+        return
+    
+    if arg != "-np":
+        get_basic_info(user)
+
+    get_contributions(user, latest)
+    get_other_activity(user, other)
+    get_stars(user, stars)
 
 
 def run():
-	if len(sys.argv) > 1:
-		print(show_contri(sys.argv[1]))
-	else:
-		print("Enter a valid username to stalk. \nFor eg: stalk aashutoshrathi")
+    if len(sys.argv) > 1:
+        if(sys.argv[1] == "--help" or sys.argv[1] == "-h"):
+            show_help()
+        # elif(sys.argv[1] == "--version"):
+        #     show_version()
+        elif len(sys.argv) == 3:
+            show_contri(sys.argv[2], (sys.argv[1]))
+        elif len(sys.argv) == 2:
+            show_contri(sys.argv[1])
+    else:
+        print("Enter a valid username to stalk. \nFor eg: stalk aashutoshrathi \nOr you can type stalk --help for help")
 
 if __name__ == '__main__':
 	run()
