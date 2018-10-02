ec09fc55623ff46c66f9b4b5e65c7284ff111563
Ana Rute Mendes <anarutecvm@gmail.com>
2018-10-01 23:18:26 -0300

    Add basic tox setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/aashutoshrathi/git-stalk-cli",
     packages=setuptools.find_packages(),
-    install_requires=['requests','prettytable', 'python-dateutil'],
+    install_requires=['requests','prettytable', 'python-dateutil', 'tox'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
