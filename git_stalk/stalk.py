3d8f5d3ea4553814bb40d66cb74ddf848f7d78cf
Grokzen <Grokzen@gmail.com>
2018-10-29 22:08:41 +0100

    Lint cleanup

--- git_stalk/stalk.py
+++ git_stalk/stalk.py
@@ -42,8 +42,10 @@ StarredRepo = namedtuple('StarredRepo', ['name', 'language', 'time'])
 
 
 def jft(user):
-    user_link = "{}{}".format(github_uri, str(user))
+    """ """
+    user_link = "{0}{1}".format(github_uri, str(user))
     response = requests.get(user_link)
+
     return response.status_code
 
 
@@ -52,27 +54,31 @@ def get_event(string):
     event = ""
     words = re.findall('[A-Z][^A-Z]*', string)
     event = " ".join(words)
+
     if event == "Pull Request Review Comment Event":
         event = "PR Review Event"
+
     if event == "Watch Event":
         event = "Starred Event"
+
     if event == "Create Event":
         event = "Commit Event"
+
     return event[:-6]
 
 
 def get_details(event):
     """Returns the details of the event according to the type of the event"""
-    types = {"IssuesEvent": event.get("payload", {}).get("issue", {}).get("title"),
-             "IssuesCommentEvent": event.get("payload", {}).get("comment", {}).get("body"),
-             "PullRequestEvent": event.get("payload", {}).get("pull_request", {}).get("title"),
-             "PushEvent": "".join([commit["message"]
-                                   for commit in event.get("payload", {}).get("commits", {}) if commit["distinct"]]),
-             "MemberEvent": "Added {} as collaborator".format(event.get("payload", {})
-                                                              .get("member", {}).get("login")),
-             "ReleaseEvent": "Released binaries for version {}".format(event.get("payload", {})
-                                                                       .get("release", {}).get("tag_name")),
-             "ForkEvent": "Forked {}".format(event.get("repo", {}.get("name")))}
+    types = {
+        "IssuesEvent": event.get("payload", {}).get("issue", {}).get("title"),
+        "IssuesCommentEvent": event.get("payload", {}).get("comment", {}).get("body"),
+        "PullRequestEvent": event.get("payload", {}).get("pull_request", {}).get("title"),
+        "PushEvent": "".join([
+            commit["message"] for commit in event.get("payload", {}).get("commits", {}) if commit["distinct"]
+        ]),
+        "MemberEvent": "Added {0} as collaborator".format(event.get("payload", {}).get("member", {}).get("login")),
+        "ReleaseEvent": "Released binaries for version {0}".format(event.get("payload", {}).get("release", {}).get("tag_name")),
+        "ForkEvent": "Forked {0}".format(event.get("repo", {}.get("name")))}
 
     return types.get(event["type"], "")
 
@@ -80,12 +86,16 @@ def get_details(event):
 def check_for_fork(link, user):
     """Check whether it is a forked."""
     tukde = link.split('/')
+
     if tukde[len(tukde) - 2] == user:
         response = requests.get(link)
         repo = response.json()
+
         if not repo["fork"]:
             return True
+
         return False
+
     return True
 
 
@@ -94,57 +104,60 @@ def get_local_time(string):
     local_time = convert_to_local(string)
     tukde = local_time.split(' ')
     samay = tukde[1].split('+')[0]
+
     return samay
 
 
 def get_following_users(user):
     """prints the users followed by current user"""
-    following_link = "{}{}/following".format(github_uri, str(user))
+    following_link = "{0}{1}/following".format(github_uri, str(user))
     following_users = requests.get(following_link).json()
 
     print("Following : ")
+
     for following_user in following_users:
-        print("{}".format(following_user["login"]))
+        print("{0}".format(following_user["login"]))
+
     print("\n")
 
 
 def get_followers(user):
     """prints the followers of user"""
-
-    followers_link = "{}{}/followers".format(github_uri, str(user))
+    followers_link = "{0}{1}/followers".format(github_uri, str(user))
     followers = requests.get(followers_link).json()
 
     print("Followed By : ")
+
     for follower in followers:
-        print("{}".format(follower["login"]))
+        print("{0}".format(follower["login"]))
+
     print("\n")
 
 
 def get_basic_info(user):
     """Prints the user's basic info."""
-
-    user_link = "{}{}".format(github_uri, str(user))
+    user_link = "{0}{1}".format(github_uri, str(user))
     user_profile = requests.get(user_link)
     profile = user_profile.json()
 
-    print("Name: {}".format(profile["name"]))
-    print("Company: {}".format(profile["company"]))
-    print("Bio: {}".format(profile["bio"]))
-    print("Followers: {}".format(profile["followers"]))
-    print("Following: {}".format(profile["following"]))
-    print("Public Repos: {}".format(profile["public_repos"]))
-    print("Public Gists: {}".format(profile["public_gists"]))
-    print("Open for hiring: {} \n".format(profile["hireable"]))
+    print("Name: {0}".format(profile["name"]))
+    print("Company: {0}".format(profile["company"]))
+    print("Bio: {0}".format(profile["bio"]))
+    print("Followers: {0}".format(profile["followers"]))
+    print("Following: {0}".format(profile["following"]))
+    print("Public Repos: {0}".format(profile["public_repos"]))
+    print("Public Gists: {0}".format(profile["public_gists"]))
+    print("Open for hiring: {0} \n".format(profile["hireable"]))
 
 
 def convert_to_local(string):
     """Returns the local_stamp as string."""
-
     from_zone = tz.tzutc()
     to_zone = tz.tzlocal()
     utc_stamp = datetime.datetime.strptime(
-        string, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=from_zone)
+        string,'%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=from_zone)
     local_stamp = utc_stamp.astimezone(to_zone)
+
     return str(local_stamp)
 
 
@@ -157,85 +170,102 @@ def date_time_validate(date_text):
 
 def get_contributions(user, latest, date_text, org=None):
     """
-        Traverses the latest array,
-        creates a table
-        if org argument is present only the repos which belong to the org \
-        is added to the table
-        and prints the table.
+    Traverses the latest array,
+    creates a table
+    if org argument is present only the repos which belong to the org \
+    is added to the table
+    and prints the table.
     """
     print("Contributions Today: ")
+
     if latest:
         table = PrettyTable(["Type", "Repository", "Time", "Details"])
+
         for event in latest:
             repo_name = event["repo"]["name"]
+
             if org:
                 curr_org = ""
+
                 for c in repo_name:
                     if c == r'/':
                         break
                     curr_org += c
+
                 if curr_org == org:
                     table.add_row([
                         get_event(event["type"]),
                         event["repo"]["name"],
                         get_local_time(event["created_at"]),
-                        get_details(event)
+                        get_details(event),
                     ])
             else:
                 table.add_row([
                     get_event(event["type"]),
                     event["repo"]["name"],
                     get_local_time(event["created_at"]),
-                    get_details(event)
+                    get_details(event),
                 ])
+
         print(table)
-    print("{} have made {} public contribution(s) {}.\n".format(
+
+    print("{0} have made {1} public contribution(s) {2}.\n".format(
         user, str(len(latest)), date_text))
 
 
 def get_other_activity(user, other, date_text):
     """
-        Traverses the other array,
-        creates a table
-        and prints the table.
+    Traverses the other array,
+    creates a table
+    and prints the table.
     """
-    print("Other Activity {}: ".format(date_text))
+    print("Other Activity {0}: ".format(date_text))
+
     if other:
         other_table = PrettyTable(["Type", "Repository", "Time", "Details"])
+
         for event in other:
             other_table.add_row([
                 get_event(event["type"]), event["repo"]["name"],
                 get_local_time(event["created_at"]),
                 get_details(event),
             ])
+
         print(other_table)
-    print("{} have done {} other public activit(y/ies) {}.\n".format(
+
+    print("{0} have done {1} other public activit(y/ies) {2}.\n".format(
         user, str(len(other)), date_text))
 
 
 def display_stars(user, stars, date_text):
     """
-        Traverses the stars array,
-        creates a table
-        and prints the table.
+    Traverses the stars array,
+    creates a table
+    and prints the table.
     """
-    print("Starred {}: ".format(date_text))
+    print("Starred {0}: ".format(date_text))
+
     if stars:
         star_table = PrettyTable(["Repository", "Language", "Time"])
+
         for starred_repo in stars:
             star_table.add_row([
                 starred_repo["repo"]["name"],
                 get_language_for_repo(starred_repo["repo"]["url"]),
-                get_local_time(starred_repo["created_at"])])
+                get_local_time(starred_repo["created_at"]),
+            ])
+
         print(star_table)
-    print("{} have starred {} repo(s) {}.".format(
+
+    print("{0} have starred {1} repo(s) {2}.".format(
         user, str(len(stars)), date_text))
 
 
 def fill_todays_data(user, today, events, latest, stars, other):
-    """Traverses the events array and separates individual data to latest,
-    stars and other arrays"""
-
+    """
+    Traverses the events array and separates individual data to latest,
+    stars and other arrays
+    """
     for event in events:
         starts_today = convert_to_local(event["created_at"]).startswith(today)
         event_type_issue_comment_event = event["type"] != "IssueCommentEvent"
@@ -247,13 +277,15 @@ def fill_todays_data(user, today, events, latest, stars, other):
                 other.append(event)
             elif check_for_fork(event["repo"]["url"], user):
                 latest.append(event)
+
     return latest, stars, other
 
 
 def fill_dated_data(user, events, latest, stars, other):
-    """Traverses the events array and seperates individual data to latest,
-    stars and other arrays"""
-
+    """
+    Traverses the events array and seperates individual data to latest,
+    stars and other arrays
+    """
     for event in events:
         event_type_issue_comment_event = event["type"] != "IssueCommentEvent"
 
@@ -264,20 +296,24 @@ def fill_dated_data(user, events, latest, stars, other):
                 other.append(event)
             elif check_for_fork(event["repo"]["url"], user):
                 latest.append(event)
+
     return latest, stars, other
 
 
 def get_language_for_repo(url):
     response = requests.get(url)
     repo = response.json()
+
     return repo['language']
 
 
 def create_star(event):
     language = get_language_for_repo(event['repo']['url'])
+
     return StarredRepo(
         name=event['repo']['name'], language=language,
-        time=get_local_time(event['created_at']))
+        time=get_local_time(event['created_at']),
+    )
 
 
 def update():
@@ -307,43 +343,45 @@ def parse_date_from_string(datetime_object):
 def get_dates_from_arguments(arguments):
     """Return triplet of dates from given arguments."""
     since_date, until_date, text_date = None, None, ""
+
     if arguments["--since"]:
         since_date = parse_date_from_string(arguments["--since"])
-        text_date = "since {}".format(since_date)
+        text_date = "since {0}".format(since_date)
+
     if arguments["--until"]:
         until_date = parse_date_from_string(arguments["--until"])
         if text_date == "":
-            text_date = "until {}".format(until_date)
+            text_date = "until {0}".format(until_date)
         else:
-            text_date = "from {} to {}".format(since_date, until_date)
+            text_date = "from {0} to {1}".format(since_date, until_date)
+
     return since_date, until_date, text_date
 
 
 def show_contri(args=None):
-    """Sends a get request to GitHub REST api and display data using the
-    utility functions"""
-
+    """
+    Sends a get request to GitHub REST api and display data using the
+    utility functions
+    """
     user = args["<name>"]
     today = str(datetime.datetime.now().strftime("%Y-%m-%d"))
-    link = "{}{}/events".format(github_uri, str(user))
+    link = "{0}{1}/events".format(github_uri, str(user))
     response = requests.get(link)
     events = response.json()
     latest = []
     stars = []
     other = []
-
     text_date = ""
+
     # NOTE: This needs more work. Possibly creating its own class of some sorts.
     if response.status_code == 200:
         since_date, until_date, text_date = get_dates_from_arguments(args)
         events = filter_since_until_dates(events, since_date=since_date, until_date=until_date)
 
         if 'since_date' in vars() or 'until_date' in vars():
-            latest, stars, other = fill_dated_data(
-                user, events, latest, stars, other)
+            latest, stars, other = fill_dated_data(user, events, latest, stars, other)
         else:
-            latest, stars, other = fill_todays_data(
-                user, today, events, latest, stars, other)
+            latest, stars, other = fill_todays_data(user, today, events, latest, stars, other)
 
         if not args["--np"]:
             get_basic_info(user)
@@ -358,17 +396,21 @@ def show_contri(args=None):
 
         if args["--followers"]:
             get_followers(user)
+
         get_other_activity(user, other, text_date)
         display_stars(user, stars, text_date)
     else:
         error_messages = {
             404: "User with username {0} does not exists, please check and try again".format(user),
             403: ("API rate limit exceeded for IP address"
-                  " {} Try again later or change IP adress.").format(getipaddress()),
+                  " {0} Try again later or change IP adress.").format(getipaddress()),
         }
-        fallback_error_message = ("Something went wrong, please check your internet connection \n"
-                                  "Use stalk --help for Help")
+        fallback_error_message = (
+            "Something went wrong, please check your internet connection \n"
+            "Use stalk --help for Help"
+        )
         err_msg = error_messages.get(response.status_code, fallback_error_message)
+
         print(err_msg)
 
 
